426eeb6d16fb39958847c30ce5b695593b305c2e
Jack Margeson <margeson.jack@gmail.com>
2020-10-03 13:53:01 -0400

    Import react-mdl

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Grid, Cell, Layout,Header, Navigation, Drawer, Footer, FooterSection, FooterLinkList } from 'react-mdl';
+import { Content } from 'react-mdl/lib/Layout';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="main">
+      <Grid>
+          <Cell col={12}>test</Cell>
+      </Grid>
     </div>
   );
 }
